051e3f0b6af9e467cf1a758477586dce6993f06e
ethiroto <squidwhich@gmail.com>
2025-08-03 17:09:28 -0700

    Update bulletin.js

--- bulletin/bulletin.js
+++ bulletin/bulletin.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         $('.alert-container').removeAttr('target');
 
         $('.alert-container').on('click',function(){
-            parent.location.href = 'https://ffm.to/started-a-war';
+            parent.location.href = 'https://ffm.to/hiro-7ules';
         })
       }
     }
